fa027926dae32b69b0f9450e348d9300c7aee6dc
Karthik kumar <mkarthikkumar19@gmail.com>
2020-06-15 19:29:19 +0530

    redux setup!

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {FlatList} from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return(
+        <FlatList></FlatList>
+    )
+}
+
+export default ProductsOverviewScreen;
