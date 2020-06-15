8d05af6619fc885b8c743f6e827b9c0eaa581287
Karthik kumar <mkarthikkumar19@gmail.com>
2020-06-15 23:15:58 +0530

    naviagtor,productOverview

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {View,Text} from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
-// import ShopNavigator from './navigation/ShopNavigator';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -15,9 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-<View>
-  <Text>Hello</Text>
-</View>
+      <ShopNavigator/>
     </Provider>
   );
 }
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {FlatList,Text} from 'react-native';
+import {useSelector} from 'react-redux';
+import ProductItem from '../../components/Shop/ProductItem';
 
 const ProductsOverviewScreen = props => {
+    const products = useSelector(state => state.products.availableProducts);
     return(
-        <FlatList></FlatList>
+        <FlatList data={products} renderItem={itemData => <ProductItem image={itemData.item.imageUrl}
+        title={itemData.item.title} price={itemData.item.price} onViewDetail={() => {}}
+        onAddToCart={() => {}} />}/>
     )
+};
+
+ProductsOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
 }
 
 export default ProductsOverviewScreen;
